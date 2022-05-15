a1b49499b47bb129c7dfc48980f88cb11c2c7f85
viniciusgribas <viniciusgribas@gmail.com>
2022-05-15 12:42:38 -0300

    CNN_News finished

--- CNN_WordCloud.py
+++ CNN_WordCloud.py
@@ -1,31 +1,73 @@
-# import the libraries
 
-from bs4 import BeautifulSoup
-import pandas as pd
-import requests
-import urllib.request
-import time
-import nltk
-from newspaper import Article
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-import datetime
+#############################################################################################
+# ──────────────────────────────────────────────────────────────────────────────────────────#
+# ─██████──██████─██████████████─████████████████───────────────────────────────────────────#
+# ─██░░██──██░░██─██░░░░░░░░░░██─██░░░░░░░░░░░░██───────────────────────────────────────────#
+# ─██░░██──██░░██─██░░██████████─██░░████████░░██───────────────────────────────────────────#
+# ─██░░██──██░░██─██░░██─────────██░░██────██░░██───────────────────────────────────────────#
+# ─██░░██──██░░██─██░░██─────────██░░████████░░██───────────────────────────────────────────#
+# ─██░░██──██░░██─██░░██──██████─██░░░░░░░░░░░░██───────────────────────────────────────────#
+# ─██░░██──██░░██─██░░██──██░░██─██░░██████░░████───────────────────────────────────────────#
+# ─██░░░░██░░░░██─██░░██──██░░██─██░░██──██░░██─────────────────────────────────────────────#
+# ─████░░░░░░████─██░░██████░░██─██░░██──██░░██████─────────────────────────────────────────#
+# ───████░░████───██░░░░░░░░░░██─██░░██──██░░░░░░██─────────────────────────────────────────#
+# ─────██████─────██████████████─██████──██████████─────────────────────────────────────────#
+# ──────────────────────────────────────────────────────────────────────────────────────────#
+# CNN_WordCloud-[15/05/2022]                                                                #
+# ──────────────────────────────────────────────────────────────────────────────────────────#
+#############################################################################################
+# Vinicius Guerra e Ribas                                                                   #
+## [Energy Engineer (UnB)] │ [Data Scientist & Analytics (USP)]                             #
+#                                                                                           #
+# github.com/viniciusgribas                                                                 #
+#############################################################################################
 
-nltk.download('punkt') # "Punkt Tokenizer Models" divides the text into a list of sentences using ML algorithm
 
-
+######################
+###### MANUAL STEPS
+######################
 
 # Number of pages to scrap
-n = 3
+## min = 1
+n = 1
 
 # search theme
+## use '-' for spacing
+## samples: 'energia' ; 'energia-renovavel' ; 'energias-renováveis-no-brasil'
 search_theme = 'energia'
 
+######################
+###### AUTOMATIC STEPS
+######################
+
+# import the libraries
+
+## WebScrapping Libraries
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+
+## NLP and ML Libraries
+import nltk
+from newspaper import Article
+nltk.download('floresta') # "Portuguese Treebank" divides the text into a list of sentences using ML algorithm
+from nltk.corpus import stopwords
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+## Data Frame manipulation and Analytics Libraries
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+import plotly_express as px
+import datetime
+
+# Create a function to scrape the website
 
 def scrape_cnn_website(search, page):
 # """ 
-# Scrapes the CNN Website based on a theme and a page number and returns a News based DataFrame.
+# Scrapes the CNN Website based on a theme and a page number.
 # """
+
     page_theme = search
     page_number = str(page)
 
@@ -34,7 +76,6 @@ def scrape_cnn_website(search, page):
     soup = BeautifulSoup(webpage.text, 'html.parser')
 
     # Get the location of the information
-
     article_date = soup.find_all('span', attrs= {'class': 'home__title__date'}) # location of data information
     article_title = soup.find_all('h2', attrs= {'class': 'news-item-header__title'} ) # location of title information
     article_tag = soup.find_all('span', attrs= {'class': 'latest__news__category'} ) # location of the tag
@@ -42,7 +83,6 @@ def scrape_cnn_website(search, page):
     article_links = soup.find_all('a',attrs={'class': 'home__list__tag'} )# location of the links
 
     # Loop through the article_date
-
     for i in article_date:
         temp=0
         temp = i.text.strip()
@@ -54,7 +94,6 @@ def scrape_cnn_website(search, page):
         dates.append(date_time)
 
     # Loop through the article_titles and set the theme
-
     for i in article_title:
         temp = 0
         temp = i.text.strip()
@@ -62,30 +101,26 @@ def scrape_cnn_website(search, page):
         theme.append(article_theme[0].text.strip().split()[2])
 
     # Loop through the article_tags
-
     for i in article_tag:
         temp = 0
         temp = i.text.strip()
         tag.append(temp)
 
     # Loop through the article_links
-
     for i in article_links:
         href = i.get('href')
         links.append(href)
 
-          # NLP Process
-
-        article = Article(href)
+        # NLP Process: Scrapes the article, download the information and parse a nlp into a interable object
+        article = Article(href) # Scrapes the Article
         article.download()
         article.parse()
         article.nlp()
 
+        # Interact with the articles
         site_name = article.meta_data['og']['site_name']
-        # title = article.meta_data['og']['title']
         text = article.text
         summary = article.summary
-        # author = article.authors
         texts.append(text)
         summarys.append(summary)
         authors.append(site_name)
@@ -100,16 +135,15 @@ links = []
 texts = []
 summarys = []
 
+# performs the scrapping with the chosen search on the given pages
+for i in range(1 , n+1):
 
-
-
-
-# applying the function
-for i in range(1 , n):
-
+    #applies the function
     scrape_cnn_website(search_theme, i )
-    print(
-    len(authors), 
+
+    # checks how many lines are created for each page
+    print(i,        # page
+    len(authors),  
     len(links), 
     len(theme), 
     len(tag), 
@@ -119,7 +153,7 @@ for i in range(1 , n):
     len(summarys) 
 )
 
-# create a Data_Frame
+# creates a dataframe based on the stored lists
 df = pd.DataFrame( columns= ['dates','theme','authors','tag','title','summarys','texts','links' ] )
 df.dates = dates
 df.dates = pd.to_datetime(df.dates,format= '%d/%m/%Y - %H:%M')
@@ -131,52 +165,47 @@ df.summarys = summarys
 df.texts = texts
 df.links = links
 
+# check the analyzed dates
 date_min = str(df.dates.dt.date.min().strftime('%d/%m/%Y'))+' at '+ str(df.dates.dt.time.min())
 date_max = str(df.dates.dt.date.max().strftime('%d/%m/%Y'))+' at '+ str(df.dates.dt.time.max())
-
 df_date_range = 'DF Date Rage: from ['+ date_min + '] to [' + date_max + ']'
-
 print(df_date_range)
 
+# checks if the dataframe has null values
+df.isnull().sum()
 
-# filter by date
-
+# filtering data from the beginning of the year
 df_filtered = df[df.dates > '2022-01-01']
 
+
+# check the analyzed dates
 date_min = str(df_filtered.dates.dt.date.min().strftime('%d/%m/%Y'))+' at '+ str(df_filtered.dates.dt.time.min())
 date_max = str(df_filtered.dates.dt.date.max().strftime('%d/%m/%Y'))+' at '+ str(df_filtered.dates.dt.time.max())
 
+# range of dates
 date_range = 'Date Rage: from ['+ date_min + '] to [' + date_max + ']'
-
 print(date_range)
 
+# data vizualization
 df_filtered
+
+# NLP: stopword parametres
+
+## github patch with Brazilian stopwords
+PORTUGUESE_STOPWORDS_PATCH_1 = r'https://gist.githubusercontent.com/alopes/5358189/raw/2107d809cca6b83ce3d8e04dbd9463283025284f/stopwords.txt' ##online patch
+response_PATCH = requests.get(PORTUGUESE_STOPWORDS_PATCH_1) ## accessing via webscrapping
+pt_stopwords = response_PATCH.text ## assigning to a variable
+pt_stopwords = pt_stopwords.replace(" ","").splitlines() ## manipulating extraction
 
 # WordCloud text and additional parameters
 
-text = " ".join(s.lower() for s in df_filtered.texts)
-wordcloud_theme = df_filtered.theme[0].lower()
-wordcloud_title = 'Author: '+ df_filtered.authors[0]+' │ Theme: ' + df_filtered.theme[0] +' │ '+ date_range     # WordCloud text and additional parameters
+text = " ".join(s.lower() for s in df_filtered.texts) # lowercasing all the text words
+wordcloud_theme = df_filtered.theme[0].lower() # get the search theme
+wordcloud_title = 'Author: '+ df_filtered.authors[0]+' │ Theme: ' + df_filtered.theme[0] +' │ '+ date_range # setting the title
 
-text = " ".join(s.lower() for s in df_filtered.texts)
-wordcloud_theme = df_filtered.theme[0].lower()
-wordcloud_title = 'Author: '+ df_filtered.authors[0]+' │ Theme: ' + df_filtered.theme[0] +' │ '+ date_range     
+## manual stopwords input
+manual_stopwords = [wordcloud_theme,
 
-
-# StopWords parameters
-
-PORTUGUESE_STOPWORDS_PATCH_1 = r'https://gist.githubusercontent.com/alopes/5358189/raw/2107d809cca6b83ce3d8e04dbd9463283025284f/stopwords.txt'
-
-
-response_PATCH = requests.get(PORTUGUESE_STOPWORDS_PATCH_1)
-
-pt_stopwords = response_PATCH.text
-
-pt_stopwords = pt_stopwords.replace(" ","").splitlines()  
-
-
-
-stop_words  = list(pt_stopwords) + list(STOPWORDS) + [wordcloud_theme,
                                                     'cerca',
                                                     'país',
                                                     'disse',
@@ -200,16 +229,56 @@ stop_words  = list(pt_stopwords) + list(STOPWORDS) + [wordcloud_theme,
                                                     'getty images',
                                                     'anadolu',
                                                     'images',
-                                                    'agency']
-                                               
+                                                    'agency',
+                                                    'ante',
+                                                    'após',
+                                                    'segunda',
+                                                    'terça',
+                                                    'quarta',
+                                                    'quinta',
+                                                    'sexta',
+                                                    'sabado',
+                                                    'domingo',
+                                                    'feira',
+                                                    'enquanto',
+                                                    'deve',
+                                                    'semana',
+                                                    'nesta',
+                                          'caso',
+                                          'diz',
+                                          'hoje',
+                                          'brasil',
+                                          'empresa',
+                                          'grande',
+                                          'fonte',
+                                          'renovável',
+                                          'afirma',
+                                          'área',
+                                          'uso',
+                                          'capacidade',
+                                          'apenas',
+                                          'partir',
+                                          'projeto',
+                                          'cnn',
+                                          'passado',
+                                          'partir',
+                                          'porque',
+                                          'custo',
+                                          'exemplo',
+                                          'especialista',
+                                          'skydweller',
+                                          'vai',
+                                          'outra',
+                                          'gera',
+                                          'loja'
+                                                   ]
 
-# Plot the WordCloud
+## stopwords used
+stop_words  = list(pt_stopwords) + list(STOPWORDS) + list(manual_stopwords) 
 
+# WordCloud NLP algorithm
 
-plt.figure(figsize=(20,10))
-
-
-
+## WordCloud Figure
 wordcloud = WordCloud(min_font_size=50, 
                max_font_size=500, 
                background_color='white', 
@@ -222,14 +291,54 @@ wordcloud = WordCloud(min_font_size=50,
                normalize_plurals= True
 ).generate(text)
 
+## WordCloud Text Count
+wordcloud_text = WordCloud(min_font_size=50, 
+               max_font_size=500, 
+               background_color='white', 
+               mode="RGB",
+               colormap='tab10',
+               stopwords=stop_words,
+               width=3000, 
+               height=1500,
+               normalize_plurals= True
+).process_text(text)
 
 
-
+# Ploting the WordCloud figure
+plt.figure(figsize=(20,10))
 plt.title(wordcloud_title, fontsize=10, color="black")
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
-plt.to_file('CNN'+search_theme+'.png') 
+
+# iterative scatter analysis with wordcloud words
+
+## creating a dataframe with the dictionary generated by the NLP algorithm
+df2 = pd.DataFrame.from_dict(wordcloud_text, orient='index') # generating a dataframe from a dictionary
+df2 = df2.rename(columns={0:'count'}) 
+df2['word'] = df2.index
+df2 = df2.reset_index(drop=True)
+df2 = df2[['word','count']]
+
+
+## generating a scatterplot from the plotfy library
+fig = px.scatter(df2.query('count > 5'),
+                y="count",
+                size="count",
+                color="word",
+                hover_name="word",
+                log_x=True,
+                title = wordcloud_title,
+                width=1500, 
+                height=750,
+                size_max=60)
+fig.show()
+
+
+
+
+
+
 
 
 
